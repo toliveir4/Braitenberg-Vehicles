a31a408c4bc65ac96f926cfde1e7f9b31df48da0
Tiago Oliveira <tiago.o.tocha@gmail.com>
2022-03-15 10:58:35 +0000

    Update

--- Assets/Scripts/LightDetectorGaussScript.cs
+++ Assets/Scripts/LightDetectorGaussScript.cs
@@ -4,18 +4,35 @@ using System.Linq;
 using System;
 
 public class LightDetectorGaussScript : LightDetectorScript {
-
 	public float stdDev = 1.0f; 
 	public float mean = 0.0f; 
 	public float min_y;
 	public bool inverse = false;
+
 	// Get gaussian output value
 	public override float GetOutput()
 	{
-		// YOUR CODE HERE
+		if (ApplyThresholds)
+		{
+			if (output < MinX || output > MaxX)
+				return 0.0f;
+		}
+		if (ApplyLimits)
+		{
+			if (output < MinY)
+				return MinY;
 
-		return 0.0f;
+			if (output > MaxY)
+				return MaxY;
+		}
+
+		// calc of Gauss Function (source: Wikipedia)
+        float A = (float) (1 / (stdDev * Math.Sqrt(2.0 * Math.PI)));
+        output = (float) (A * Math.Exp(-Math.Pow(output - mean, 2) / (2 * Math.Pow(stdDev, 2))));
+
+		if (inverse)
+			return 1 - output;
+		else
+			return output;
 	}
-
-
 }
