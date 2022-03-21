16941a48ef640b83b1635f07abb08ba07a9cbfe7
Tiago Oliveira <tiago.o.tocha@gmail.com>
2022-03-21 21:33:40 +0000

    meta1 e elipse

--- Assets/Scripts/CarDetectorGaussScript.cs
+++ Assets/Scripts/CarDetectorGaussScript.cs
@@ -8,12 +8,14 @@ public class CarDetectorGaussScript : CarDetectorScript
 
     public float stdDev = 0.1f;
     public float mean = 0.0f;
+	public float response;
+
     // Get gaussian output value
     public override float GetOutput()
     {
 		// calc of Gauss Function (source: Wikipedia)
 		float A = (float)(1 / (stdDev * Math.Sqrt(2.0 * Math.PI)));
-		float response = (float)(A * Math.Exp(-Math.Pow(output - mean, 2) / (2 * Math.Pow(stdDev, 2))));
+		response = (float)(A * Math.Exp(-Math.Pow(output - mean, 2) / (2 * Math.Pow(stdDev, 2))));
 
 		if (ApplyThresholds)
 		{
--- Assets/Scripts/LightDetectorGaussScript.cs
+++ Assets/Scripts/LightDetectorGaussScript.cs
@@ -8,13 +8,14 @@ public class LightDetectorGaussScript : LightDetectorScript {
 	public float mean = 0.0f; 
 	public float min_y;
 	public bool inverse = false;
+	public float response;
 
 	// Get gaussian output value
 	public override float GetOutput()
 	{
 		// calc of Gauss Function (source: Wikipedia)
         float A = (float) (1 / (stdDev * Math.Sqrt(2.0 * Math.PI)));
-        float response = (float) (A * Math.Exp(-Math.Pow(output - mean, 2) / (2 * Math.Pow(stdDev, 2))));
+        response = (float) (A * Math.Exp(-Math.Pow(output - mean, 2) / (2 * Math.Pow(stdDev, 2))));
 
 		if (ApplyThresholds)
 		{
